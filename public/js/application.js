a606ed8d4417dce8e88fda3a522de7c5561c41ca
Shary, STeve <stephen.shary@gmail.com>
2015-01-01 19:34:58 -0500

    adding in more basic routing.

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,16 @@
 
 angular.module('homeController', []).controller('homeController', ['$scope', '$location', homeController]);
 angular.module('loginController', []).controller('loginController', ['$scope', '$location', loginController]);
+angular.module('leaderBoardController', []).controller('leaderBoardController', ['$scope', '$location', leaderBoardController]);
 
 
-var trumperyApp = angular.module('trumperyApp', ['ngRoute', 'homeController', 'loginController']);
+var trumperyApp = angular.module('trumperyApp', ['ngRoute', 'homeController', 'loginController', 'leaderBoardController']);
 
 trumperyApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'homeController'});
-        $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginController'});
-
+        $routeProvider.when('/join', {templateUrl: 'partials/join.html', controller: 'loginController'});
+        $routeProvider.when('/leaderboard', {templateUrl: 'partials/leaderboard.html', controller: 'leaderBoardController'});
+        $routeProvider.when('/joinWatch', {templateUrl: 'partials/joinWatch.html', controller: 'loginController'});
     }]);
 
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,11 +1,28 @@
 var homeController = function ($scope, $location) {
-    $scope.gotoLogin = function () {
-        $location.path('/login');
+    $scope.gotoJoin = function () {
+        $location.path('/join');
+    };
+
+    $scope.gotoJoinWatch = function () {
+        $location.path('/joinWatch');
+    };
+
+    $scope.gotoLeaderBoard = function() {
+        $location.path('/leaderboard');
     };
 };
 
 var loginController = function ($scope, $location) {
+    $scope.gameCode = "";
     $scope.cancel = function () {
         $location.path('/');
     };
+
+    $scope.watch = function() {
+        $location.path('/watch');
+    };
+};
+
+var leaderBoardController = function() {
+
 };
