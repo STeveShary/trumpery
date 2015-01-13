bd7a1f0516470be7eabb90a8857036f1959aace4
Zach Thacker <zpthacker@gmail.com>
2015-01-13 13:59:50 -0500

    Zach and Gregg | Adding major routes to get questions and submit answers

--- app/controller/gameController.js
+++ app/controller/gameController.js
@@ -21,13 +21,16 @@ var isValidJoinRequest = function (request) {
 };
 
 exports.joinGame = function (request) {
-    return dbService.isValidGame(request.body.gameCode).then(function (isValidGame) {
-        if (isValidGame && isValidJoinRequest(request)) {
+    return dbService.isValidGame(request.body.gameCode).then(function (game) {
+        if (game && isValidJoinRequest(request)) {
             var participantCode = buildGuid().toUpperCase();
             var nonEmptyPlayers = request.body.players.filter(function(player) { return player != undefined && player.length > 0});
             dbService.setupParticipant(request.body.gameCode, participantCode, request.body.teamName,
                 nonEmptyPlayers, true);
             return participantCode;
         }
+    }).fail(function() {
+      console.log("not a valid game");
+      return null;
     });
 };
